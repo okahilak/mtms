3eb90694d324c0e56acd08ebcdcc34fcba11e524
Olli-Pekka Kahilakoski <okahilak@gmail.com>
2026-03-22 09:24:46 +0200

    MOD: Hide mTMS simulator under feature flag in .env

--- src/mtms_simulator/ros_entrypoint.sh
+++ src/mtms_simulator/ros_entrypoint.sh
@@ -4,4 +4,9 @@ set -e
 source /opt/ros/jazzy/setup.bash
 source /app/install/setup.bash
 
+if [ "$USE_MTMS_SIMULATOR" != "true" ]; then
+  echo "USE_MTMS_SIMULATOR is not set to true, exiting."
+  exit 0
+fi
+
 ros2 launch mtms_simulator mtms_simulator.launch.py log-level:="$ROS_LOG_LEVEL"
