f43b8af572566123108c639ff333f6c87a7ffa0d
Olli-Pekka Kahilakoski <okahilak@gmail.com>
2026-03-22 17:04:38 +0200

    MOD: Run busylight manager with ros2 launch

--- src/busylight_manager/ros_entrypoint.sh
+++ src/busylight_manager/ros_entrypoint.sh
@@ -4,4 +4,4 @@ set -e
 source /opt/ros/jazzy/setup.bash
 source /app/install/setup.bash
 
-ros2 run busylight_manager start
+ros2 launch busylight_manager busylight_manager.launch.py log-level:="$ROS_LOG_LEVEL"
--- src/busylight_manager/setup.py
+++ src/busylight_manager/setup.py
@@ -1,4 +1,6 @@
 from setuptools import setup
+import os
+from glob import glob
 
 package_name = 'busylight_manager'
 
@@ -7,6 +9,8 @@ setup(
     version='0.1.0',
     packages=[package_name],
     data_files=[
+        (os.path.join('share', package_name, 'launch'),
+         glob(os.path.join('launch', '*.launch.py'))),
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
