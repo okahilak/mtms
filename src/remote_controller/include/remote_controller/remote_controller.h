ccd23c2f40b1e0ee18621519fd8e20ce8ee8ec67
Olli-Pekka Kahilakoski <okahilak@gmail.com>
2026-03-23 20:13:39 +0200

    FIX: Concurrency issue in remote controller

--- src/remote_controller/include/remote_controller/remote_controller.h
+++ src/remote_controller/include/remote_controller/remote_controller.h
@@ -96,7 +96,7 @@ private:
   std::mutex state_mutex;
 
   // Trial readiness state.
-  bool prepare_trial_ongoing{false};
+  std::atomic<bool> prepare_trial_ongoing{false};
 
   // Prevent overlapping trials.
   std::atomic<bool> trial_ongoing{false};
